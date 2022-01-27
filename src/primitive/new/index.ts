5f91eb927e91481f6274b9cd121c47b2a4603ee2
Rain120 <1085131904@qq.com>
2022-01-27 11:58:10 +0800

    feat(primitive): new

--- src/primitive/new/index.ts
+++ src/primitive/new/index.ts
@@ -2,16 +2,26 @@
  * @Author: Rainy
  * @Date: 2019-11-14 19:25:01
  * @LastEditors: Rainy
- * @LastEditTime: 2019-11-24 20:52:08
+ * @LastEditTime: 2022-01-24 11:37:39
  */
+
 export function _new(...arg: any): any {
+  // Eg: class Person {} or function Person() {}
+  // Person.prototype
+  // Person.prototype.constructor === Person
+  // person = new Person => person.__proto__ === Person.prototype
+
+  // Array.prototype.shift.call(arg) => [...arg].shift()
+
   // 1
-  let obj: any = new Object();
-  let _constructor = Array.prototype.shift.call(arg);
+  // let obj: any = new Object();
   // 2
-  obj.__proto__ = _constructor.prototype;
+  // obj.__proto__ = _constructor.prototype;
+  // 1 2
+  const _constructor = Array.prototype.shift.call(arg);
+  const obj = Object.create(_constructor.prototype);
   // 3
-  let result = _constructor.apply(obj, arg);
+  const result = _constructor.apply(obj, arg);
   // 4
   return typeof result === 'object' && result !== null ? result : obj;
 }
